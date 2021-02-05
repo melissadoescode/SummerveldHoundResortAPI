ccbede1c96015789ae2916dcf3ee4e19dcc8e9ac
Melissa Govender <MelissaG@hollywoodbets.net>
2021-02-05 07:47:29 +0200

    Feat: Added StatupIoC to Startup

--- SummerveldHoundResort.WebAPI/Startup.cs
+++ SummerveldHoundResort.WebAPI/Startup.cs
@@ -33,7 +33,8 @@ namespace SummerveldHoundResort.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            StartupIoC.Setup(services)
+                ;
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
